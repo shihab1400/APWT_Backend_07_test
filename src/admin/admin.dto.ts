f6f59b966ee9f06f3f9ae1b213d70ece05bf5b0d
Farhat <shihabcoc39@gmail.com>
2026-06-27 20:56:21 +0600

    Validation with pipes and file upload with multer

--- src/admin/admin.dto.ts
+++ src/admin/admin.dto.ts
@@ -1,5 +1,25 @@
+import { IsAlpha, IsNotEmpty, Matches, Length } from 'class-validator';
+
 export class AdminDTO {
+  id: number;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsAlpha(undefined, { message: 'Name must contain only alphabets' })
   name?: string;
-  uname?: string;
-  password?: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @IsNotEmpty({ message: 'Email address field is required' })
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.xyz$/, {
+    message: 'Email must be a valid address and use the .xyz domain',
+  })
+  email?: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @Length(10, 10, { message: 'Length of NID must be 10' })
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  @Matches(/^\d+$/, { message: 'NID must contain only numeric digits' })
+  nid?: string;
+
+  profilePic?: string;
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,31 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { AdminDTO } from './admin.dto';
 
 @Injectable()
 export class AdminService {
-  getAllAdmin(): object {
-    return {
-      name: 'ShihabAllAdmin',
-      id: 52028,
-    };
+  adminData: AdminDTO[] = [
+    {
+      id: 1,
+      name: 'Shihab',
+      email: 'shihab@gmail.xyz',
+      nid: '2312345678',
+      profilePic: '12334sdwe',
+    },
+    {
+      id: 2,
+      name: 'Farhat',
+      email: 'farhat@gmail.xyz',
+      nid: '1234125623',
+      profilePic: '23sfdwqed',
+    },
+  ];
+
+  getAllAdmin(): AdminDTO[] {
+    return this.adminData;
   }
 
-  getAdminById(id: number): object {
-    return {
-      name: 'ShihabById',
-      age: 12,
-      id: id,
-    };
+  getAdminById(id: number): AdminDTO {
+    const admin = this.adminData.find((i) => i.id === id);
+    if (!admin) {
+      throw new NotFoundException(`Admin with ID ${id} not found`);
+    }
+    return admin;
   }
 
-  getAdminByQuery(id: number, name: string): object {
-    return {
-      name: `${name}ByQuery`,
-      id: id,
-    };
+  getAdminByQuery(name: string, email: string): AdminDTO[] {
+    const filteredAdmins = this.adminData.filter(
+      (n) =>
+        n.name?.toLowerCase() === name.toLowerCase() &&
+        n.email?.toLowerCase() === email.toLowerCase(),
+    );
+
+    if (filteredAdmins.length === 0) {
+      throw new NotFoundException(
+        `Admin with name "${name}" and email "${email}" not found`,
+      );
+    }
+
+    return filteredAdmins;
   }
 
   postAdminByBody(data: AdminDTO): AdminDTO {
+    const ex = this.adminData.find((i) => i.id === data.id);
+    if (ex) {
+      throw new NotFoundException(`Admin with ID "${data.id}" already exists`);
+    }
+    this.adminData.push(data);
     return data;
+  }
+
+  updateAdmin(id: number, adminObj: AdminDTO): AdminDTO {
+    const adminIndex = this.adminData.findIndex((admin) => admin.id === id);
+    if (adminIndex === -1) {
+      throw new NotFoundException(`Admin with ID ${id} not found`);
+    }
+    this.adminData[adminIndex] = adminObj;
+    return adminObj;
   }
 }
